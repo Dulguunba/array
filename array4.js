4b84f19cd7e9ac1799ed1735c2ed98363a382b3b
Dulguun Batbaatar <23LP7194@G304-CC0064.local>
2023-11-03 19:19:48 +0800

    pm

--- array4.js
+++ array4.js
@@ -1,6 +1,6 @@
 //chunk
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
-let insert = 4;
+let insert = 3;
 let emptyArr = [];
 
 for (let i = 0; i < numbers.length / insert; i++) {
--- array6.js
+++ array6.js
@@ -1,3 +1,5 @@
+//sort an array
+
 let numbers = [5, 4, 3, 2, 1];
 
 for (let i = 0; i < numbers.length; i++) {
