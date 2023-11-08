08e66e09680bb9505f7bf9f72eaa4413ceb383cf
Dulguun Batbaatar <23LP7194@G304-CC0064.local>
2023-11-08 19:54:02 +0800

    pm after lecture

--- array_hard1.js
+++ array_hard1.js
@@ -1,26 +1,27 @@
 // array hard task 1 = find longest increasing subsequence
 let massive = [0, 1, 0, 3, 2, 3];
-
+var numberofLegth = [];
 for (let i = 0; i < massive.length; i++) {
   let reserve = [];
-  let legth = [];
+  let legth = 0;
   reserve.unshift(massive[i]);
   for (let k = i; k < massive.length; k++) {
     if (massive[k] < massive[k + 1]) {
       reserve.push(massive[k + 1]);
     }
   }
-  legth.push(reserve.legth);
+  legth = reserve.length;
+  numberofLegth.push(legth);
 }
 
-// for (let j = 0; j < legth.legth; j++) {
-//   let maxlegth = 0;
-//   for (let n = 0; n < legth.legth; n++) {
-//     if (legth[j] >= legth[n]) {
-//       maxlegth++;
-//     }
-//   }
-//   if (maxlegth == legth.legth) {
-//     console.log(reserve[j]);
-//   }
-// }
+for (let j = 0; j < numberofLegth.length; j++) {
+  let score = 0;
+  for (let n = 0; n < numberofLegth.length; n++) {
+    if (numberofLegth[j] >= numberofLegth[n]) {
+      score++;
+    }
+  }
+  if (score == numberofLegth.length) {
+    console.log(numberofLegth[j]);
+  }
+}
